a4a6486a2a9ed73c08e2d0c1e052a94d0ee2b4c4
Kingsley <kingsleyumujeyan@gmail.com>
2020-12-05 19:42:51 +0100

    ft--updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
+import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 import './App.css';
+
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid textAlign="center">
+      <Header/>
+      <Hero />
+      <Footer />
+    </Container>
   );
 }
 
