711cd1c0277c2010c98a147b281dd4f38bd8cbe4
Kingsley <kingsleyumujeyan@gmail.com>
2020-12-05 22:13:05 +0100

    ft--added authentication for signup and connected to backend api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "semantic-ui-css/semantic.min.css";
 import './App.css';
 
+import { AuthProvider } from "./context/auth";
+
+
 import Header from "./components/Header";
-import Hero from "./components/Hero";
+import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 
 
 function App() {
   return (
-    <Container fluid textAlign="center">
-      <Header/>
-      <Hero />
-      <Footer />
-    </Container>
+    <AuthProvider>
+      <Router>
+        <Container fluid textAlign="center">
+          <Header/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
+          <Footer />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
